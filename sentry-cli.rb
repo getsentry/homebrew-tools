258a368d908c219e5a2c7f32a9032834d72839f4
Daniel Griesser <daniel.griesser.86@gmail.com>
2017-02-22 13:54:36 +0100

    Update sentry-cli.rb
    Bump url to 0.25

--- sentry-cli.rb
+++ sentry-cli.rb
@@ -1,7 +1,7 @@
 class SentryCli < Formula
   desc "This is a Sentry command-line client for some generic tasks."
   homepage "https://github.com/getsentry/sentry-cli"
-  url "https://github.com/getsentry/sentry-cli/releases/download/0.24.0/sentry-cli-Darwin-x86_64"
+  url "https://github.com/getsentry/sentry-cli/releases/download/0.25.0/sentry-cli-Darwin-x86_64"
   version "0.25.0"
   sha256 "3162b500173ba6a20d9563ec7dce199625e7dbc637eccc44304af3a8b13f4569"
 
