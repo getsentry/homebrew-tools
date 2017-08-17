2900a842ddf809af33e15dcbb8531af04a716733
Armin Ronacher <armin.ronacher@active-4.com>
2017-08-17 15:30:47 +0200

    sentry-cli 1.19.0

--- sentry-cli.rb
+++ sentry-cli.rb
@@ -1,9 +1,9 @@
 class SentryCli < Formula
   desc "This is a Sentry command-line client for some generic tasks."
   homepage "https://github.com/getsentry/sentry-cli"
-  url "https://github.com/getsentry/sentry-cli/releases/download/1.18.0/sentry-cli-Darwin-x86_64"
-  version "1.18.0"
-  sha256 "8dd858a2c03a63f46381b2dc1a1d399cd8e97cc77917f72be133a12530c6070a"
+  url "https://github.com/getsentry/sentry-cli/releases/download/1.19.0/sentry-cli-Darwin-x86_64"
+  version "1.19.0"
+  sha256 "542e3f44d77b533f066938792b88b735e59d3c91faed123d32f449ee3e6edfa2"
 
   def install
     mv "sentry-cli-Darwin-x86_64", "sentry-cli"
