881081f10c733fcea18783c639c61def55e2b5f6
Jan Michael Auer <jan.auer@sentry.io>
2017-11-03 14:29:36 +0100

    ref: Remove old and unused scripts

--- scripts/upgrade-formula.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ ! -d .venv ]; then
-  virtualenv .venv
-  .venv/bin/pip install requests==2.10.0
-  .venv/bin/pip install Jinja2
-fi
-
-.venv/bin/python ./scripts/bump-formula.py
