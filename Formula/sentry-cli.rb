bc37eb23cc49fdbb37387147001973d850f18bdd
Armin Ronacher <armin.ronacher@active-4.com>
2018-01-16 20:23:38 +0100

    revert: "release: sentry-cli 1.28.2"
    This reverts commit 5651b4889beb2eda322824bf04ba0ac308de7b91.

--- Formula/sentry-cli.rb
+++ Formula/sentry-cli.rb
@@ -1,9 +1,9 @@
 class SentryCli < Formula
   desc "This is a Sentry command-line client for some generic tasks."
   homepage "https://github.com/getsentry/sentry-cli"
-  url "https://github.com/getsentry/sentry-cli/releases/download/1.28.2/sentry-cli-Darwin-x86_64"
-  version "1.28.2"
-  sha256 "ca7f8145bd79c3f67879a2d94e8e075ccf8c18ba204153b94620167c40de6d4b"
+  url "https://github.com/getsentry/sentry-cli/releases/download/1.28.1/sentry-cli-Darwin-x86_64"
+  version "1.28.1"
+  sha256 "88cb1ee4c61a86a6d9b798fe8efb9085d160f2eb7d240581a40071fbcaf50f1c"
 
   def install
     mv "sentry-cli-Darwin-x86_64", "sentry-cli"
