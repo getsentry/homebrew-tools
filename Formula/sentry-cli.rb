2de0f6bf02913245d60b2b324bf9ed444b060007
Anton Ovchinnikov <anton@tonyo.info>
2018-08-15 00:41:11 +0300

    Revert "release: sentry-cli 1.35.0"
    This reverts commit 5b4422546ac7fb0e1e14b4ceaa8b462724bf6f87.

--- Formula/sentry-cli.rb
+++ Formula/sentry-cli.rb
@@ -1,9 +1,9 @@
 class SentryCli < Formula
   desc "This is a Sentry command-line client for some generic tasks."
   homepage "https://github.com/getsentry/sentry-cli"
-  url "https://github.com/getsentry/sentry-cli/releases/download/1.35.0/sentry-cli-Darwin-x86_64"
-  version "1.35.0"
-  sha256 "6fdc3519e8877582861ba0f36af005f9005d9cd1dcd79ee236687bb9c9211ceb"
+  url "https://github.com/getsentry/sentry-cli/releases/download/1.34.0/sentry-cli-Darwin-x86_64"
+  version "1.34.0"
+  sha256 "6563294ae9cdd2d8258732295e6d2b3f5ff162e08cbc625f1774df7510bb7d8b"
 
   def install
     mv "sentry-cli-Darwin-x86_64", "sentry-cli"
