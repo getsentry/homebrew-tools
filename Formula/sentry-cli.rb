c39f73b1598450c63a01d210a6d14e4730695ad0
Kamil Ogórek <kamil@sentry.io>
2021-02-25 11:14:01 +0100

    release: sentry-cli 1.63.0

--- Formula/sentry-cli.rb
+++ Formula/sentry-cli.rb
@@ -1,18 +1,34 @@
 class SentryCli < Formula
   desc "Sentry command-line client for some generic tasks"
   homepage "https://github.com/getsentry/sentry-cli"
-  version "1.62.0"
+  version "1.63.0"
   license "BSD-3-Clause"
 
   if OS.mac?
-    url "https://downloads.sentry-cdn.com/sentry-cli/1.62.0/sentry-cli-Darwin-universal"
-    sha256 "56e5848c5ce08ef54b9853ad1fcd7c350c7115249489a106b8d49e4f6dc28c63"
-  elsif Hardware::CPU.is_64_bit?
-    url "https://downloads.sentry-cdn.com/sentry-cli/1.62.0/sentry-cli-Linux-x86_64"
-    sha256 "574ae79c5479e0cda1bcf60051f321551bf2699bae62b3c5ce4aeca96d7bacbc"
+    url "https://downloads.sentry-cdn.com/sentry-cli/1.63.0/sentry-cli-Darwin-universal"
+    sha256 "5e6aa63d56805e5b8c53c481d67ea84e9c7c1d416b159ff0c50062368f78f638"
+  elsif OS.linux?
+    if Hardware::CPU.arm?
+      if Hardware::CPU.is_64_bit?
+        url "https://downloads.sentry-cdn.com/sentry-cli/1.63.0/sentry-cli-Linux-aarch64"
+        sha256 "c922abae3281c8a6c90cf434593ac2d2d4fad4daf823bd1580b11888c1e4650f"
+      else
+        url "https://downloads.sentry-cdn.com/sentry-cli/1.63.0/sentry-cli-Linux-armv7"
+        sha256 "7d8398c8f6079499408743155015fb5da3f1848a25aa630513e5c756eef266a4"
+      end
+    elseif Hardware::CPU.intel?
+      if Hardware::CPU.is_64_bit?
+        url "https://downloads.sentry-cdn.com/sentry-cli/1.63.0/sentry-cli-Linux-x86_64"
+        sha256 "aa19e3c4138c910409a1e1b17a5ff9c57e3d6d03a338be1821603ff9b898898a"
+      else
+        url "https://downloads.sentry-cdn.com/sentry-cli/1.63.0/sentry-cli-Linux-i686"
+        sha256 "e98496b1e1eb211d4a3e949ae505845135c8c2ad9d6fe8824e50af139a4be2a4"
+      end
+    else
+      raise "Unsupported architecture"
+    end
   else
-    url "https://downloads.sentry-cdn.com/sentry-cli/1.62.0/sentry-cli-Linux-i686"
-    sha256 "c386590cf9eeb7c04ad08ad9fa8fffa801988536ba2c311a8a6e69241da9fc09"
+      raise "Unsupported operating system"
   end
 
   def install
